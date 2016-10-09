d4f8a9fec9c833c648d24ab4f5362699f6339259
Sukhyun Baik <neonagx@gmail.com>
2016-10-08 21:51:49 -0700

    added title page, main interface

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
-console.log("connected")
 
-$('body').append('<ul>What is it?</ul>')
+
+$('button').on('click',function mainGame() {
+  $('body').html('<div id="mainScreen"></div>');
+  var $screen = $('#mainScreen');
+  $screen.html('<p>It is a world of General Assembly where an adventurer goes to become the Three Weeks Master.<p>')
+  $screen.append('<button id="scroll1">Next --></button>')
+})
+
+$('body').on('click','#scroll1', function next() {
+  var $screen = $('#mainScreen');
+  $screen.html('<p>he or she must go through three trials of defeating the HTML, CSS and Javascript monsters</p>');
+  $screen.append('<button id="scroll2">Next--></button>')
+})
+
+$('body').on('click','#scroll2', function next() {
+  var $screen = $('#mainScreen');
+  $screen.html('<p>he or she must go through three trials of defeating the HTML, CSS and Javascript monsters</p>');
+  $screen.append('<button id="scroll2">Next--></button>')
+})
