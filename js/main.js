be9eb152d9fde2452a77b487c22f5f674717e98d
Sukhyun Baik <neonagx@gmail.com>
2016-10-08 22:12:45 -0700

    edited picture and questionnaire page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-
-
 $('button').on('click',function mainGame() {
   $('body').html('<div id="mainScreen"></div>');
   var $screen = $('#mainScreen');
@@ -15,6 +13,7 @@ $('body').on('click','#scroll1', function next() {
 
 $('body').on('click','#scroll2', function next() {
   var $screen = $('#mainScreen');
-  $screen.html('<p>he or she must go through three trials of defeating the HTML, CSS and Javascript monsters</p>');
-  $screen.append('<button id="scroll2">Next--></button>')
+  $screen.html('<img src="https://www.colourbox.com/preview/6415001-3d-cartoon-green-monster.jpg" alt="HTML Monter">');
+  $screen.html('<div id="question1">What is HTML?</div>')
+  $screen.append('<button id="Ques1">Next--></button>', '<button id="Ques2">Next--></button>', '<button id="Ques3">Next--></button>','<button id="Ques4">Next--></button>')
 })
