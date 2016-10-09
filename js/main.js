53f43b1a56eea0fd6cb36aaa6e920bbf3b28f705
Sukhyun Baik <neonagx@gmail.com>
2016-10-08 23:06:03 -0700

    added second question to the game

--- js/main.js
+++ js/main.js
@@ -15,5 +15,37 @@ $('body').on('click','#scroll2', function next() {
   var $screen = $('#mainScreen');
   $screen.html('<div id="question1">What is HTML?</div>')
   $('body').prepend('<img id="htmlMon" src="https://www.colourbox.com/preview/6415001-3d-cartoon-green-monster.jpg" alt="HTML Monter">');
-  $screen.append('<button id="Ques1">Hyper Text Mess Link</button>', '<button id="Ques2">Hyper Tremor Message Loss</button>', '<button id="Ques3">Hyper Text Mixed Language</button>','<button id="Ques4">Hypo Text Mixed Language</button>');
+  $screen.append('<div id="questionnaire"><button id="Ques1">Hyper Text Mess Link</button> <button id="Ques2">Hyper Tremor Message Loss</button><button id="Ques3">Hyper Text Mixed Language</button><button id="Ques4">Hypo Text Mixed Language</button></div>');
+})
+
+$('body').on('click','#Ques1', function() {
+  //alert later
+  console.log('Wrong! Monster has noticed you!!');
+  var $screen = $('#mainScreen');
+  $screen.append('<div id="question2">What must be in the header?</div>');
+  $screen.html('<div id="questionnaire"><button id="Ques1">dunno</button> <button id="Ques2">What</button><button id="Ques3">three</button><button id="Ques4">title</button></div>');
+})
+
+$('body').on('click','#Ques2', function() {
+  //alert later
+  console.log('You are correct!! Damage 1 to the monster!');
+  var $screen = $('#mainScreen');
+  $screen.html('<div id="question2">What must be in the header?</div>');
+  $screen.html('<div id="questionnaire"><button id="Ques1">dunno</button> <button id="Ques2">What</button><button id="Ques3">three</button><button id="Ques4">title</button></div>');
+})
+
+$('body').on('click','#Ques3', function() {
+  //alert later
+  console.log('Wrong! Monster has noticed you!!');
+  var $screen = $('#mainScreen');
+  $screen.html('<div id="question2">What must be in the header?</div>');
+  $screen.html('<div id="questionnaire"><button id="Ques1">dunno</button> <button id="Ques2">What</button><button id="Ques3">three</button><button id="Ques4">title</button></div>');
+})
+
+$('body').on('click','#Ques4', function() {
+  //alert later
+  console.log('Wrong! Monster has noticed you!!');
+  var $screen = $('#mainScreen');
+  $screen.html('<div id="question2">What must be in the header?</div>');
+  $screen.html('<div id="questionnaire"><button id="Ques1">dunno</button> <button id="Ques2">What</button><button id="Ques3">three</button><button id="Ques4">title</button></div>');
 })
