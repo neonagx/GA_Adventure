ef6b8bb1622fcffbc6a419ea542109e194f26056
Sukhyun Baik <neonagx@gmail.com>
2016-10-10 13:11:58 -0700

    added more intro to the game

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $('button').on('click',function mainGame() {
   $('body').html('<div id="mainScreen"></div>');
   var $screen = $('#mainScreen');
-  $screen.html('<p>It is a world of General Assembly where an adventurer goes to become the Three Weeks Master.<p>')
+  $screen.html('<p id="intro">It is a world of General Assembly where an adventurer goes to become the Three Weeks Master.<p>')
   $screen.append('<button id="scroll1">Next --></button>')
 })
 
@@ -10,14 +10,21 @@ $('body').on('click','#scroll1', function next() {
   $screen.html('<p>he or she must go through three trials of defeating the HTML, CSS and Javascript monsters</p>');
   $screen.append('<button id="scroll2">Next--></button>')
 })
-//Question 1 What is HTML?
+
 $('body').on('click','#scroll2', function next() {
+  var $screen = $('#mainScreen');
+  $screen.html('<p>Journey will not be an easy one.  Through the peril there a path to victory.  Good luck!!</p>');
+  $screen.append('<button id="scroll3">Next--></button>')
+})
+
+//Question 1 What is HTML?
+$('body').on('click','#scroll3', function next() {
   var $screen = $('#mainScreen');
   $screen.html('<div id="questions">What is HTML?</div>')
   $('body').prepend('<img id="htmlMon" src="https://www.colourbox.com/preview/6415001-3d-cartoon-green-monster.jpg" alt="HTML Monter">');
-  $screen.append('<div id="questionnaire"><button class="wrong1">Hyper Text Mess Link</button> <button class="wrong1">Hyper Tremor Message Loss</button><button class="right1">Hyper Text Mixed Language</button><button class="wrong1">Hypo Text Mixed Language</button></div>');
+  $screen.append('<div id="questionnaire"><button class="wrong1">Hyper Text Mess Link</button> <button class="wrong1">Hyper Tremor Message Loss</button><button class="right1">Hyper Text Markup Language</button><button class="wrong1">Hypo Text Mixed Language</button></div>');
 })
-
+//Question 2 What must be in the Header?
 $('body').on('click', '.wrong1', function() {
   console.log('Wrong! Monster has noticed you!!');
   var $question = $('#questions');
@@ -32,7 +39,7 @@ $('body').on('click', '.right1', function() {
   $question.text('What must be in the Header?');
   $('#questionnaire').html('<button class="right2">title</button><button class="wrong2">script</button><button class="wrong2">body</button><button class="wrong2">div</button>')
 })
-// // Question 2 Inserting Image
+// // Question 3 Inserting Image
 $('body').on('click', '.wrong2', function() {
   console.log('Monster has been enraged!!');
   var $question = $('#questions');
@@ -50,46 +57,12 @@ $('body').on('click', '.right2', function() {
   $('#wrong31').text("<image>image</image>")
   $('#right31').text("<img src='url'>")
 })
-// $('body').on('click', '.wrong2', function() {
-//   //alert later
-//   console.log('damage 1 to monster');
-//   var $question = $('#questions');
-//   $question.text('How do you insert image to the body?');
-//   $('#Ques1').text('insert image')
-//   $('#Ques2').text('<image>image</image>')
-//   $('#Ques3').text('<img src="url">')
-//   $('#Ques4').text('copy and paste image url to the body')
-// })
-//
-// $('body').on('click', '#Ques2', function() {
-//   //alert later
-//   console.log('Monster has taken out his claws!');
-//   var $question = $('#questions');
-//   $question.text('How do you insert image to the body?');
-//   $('#Ques1').text('insert image')
-//   $('#Ques2').text('<image>image</image>')
-//   $('#Ques3').text('<img src="url">')
-//   $('#Ques4').text('copy and paste image url to the body')
-// })
-//
-// $('body').on('click', '#Ques3', function() {
-//   //alert later
-//   console.log('Monster has taken out his claws!');
-//   var $question = $('#questions');
-//   $question.text('How do you insert image to the body?');
-//   $('#Ques1').text('insert image')
-//   $('#Ques2').text('<image>image</image>')
-//   $('#Ques3').text('<img src="url">')
-//   $('#Ques4').text('copy and paste image url to the body')
-// })
-//
-// $('body').on('click', '#Ques4', function() {
-//   //alert later
-//   console.log('Monster has taken out his claws!');
-//   var $question = $('#questions');
-//   $question.text('How do you insert image to the body?');
-//   $('#Ques1').text('insert image')
-//   $('#Ques2').text('<image>image</image>')
-//   $('#Ques3').text('<img src="url">')
-//   $('#Ques4').text('copy and paste image url to the body')
-// })
+
+//Question 4 On Atom/Sublime text editor, how do you bring up Doctype html in index. html?
+
+$('body').on('click', '.wrong3', function() {
+  console.log('Monster took out his sharp claw from his hands');
+  var $question = $('#questions');
+  $question.text('On Atom/Sublime text editor, how do you bring up DOCTYPE! html in the index.html?');
+  $('#questionnaire').html('<button class="wrong4">typing html and pressing enter</button><button class="wrong4">It should just summon html by itself</button><button class="wrong4">HTML should already exit</button><button class="right4">typing html and pressing tab</button>')
+})
